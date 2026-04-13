f99c8dfc26dcbbb7899cde2c47ace6cc27e50783
alessiocostanza3-ctrl <alessiocostanza3@gmail.com>
2026-04-13 19:10:49 +0200

    fix: logo.png aggiunto a SHELL_ASSETS SW (precache garantito), bump SW v238

--- sw.js
+++ sw.js
@@ -9,13 +9,14 @@
 var GAS_URL = 'https://script.google.com/macros/s/AKfycbyVMV9MkGiqphN0AKXJdHXF0Arp1vxTYrCYi1SGv_4MKLRJkx--5HoGq7mmQX-p0ZTZ/exec';
 var APP_URL = 'https://alessiocostanza3-ctrl.github.io/gestion-produzione-ombre/';
 
-var SHELL_CACHE = 'prod-shell-v237';
+var SHELL_CACHE = 'prod-shell-v238';
 var SHELL_ASSETS = [
     APP_URL,
     APP_URL + 'index.html',
     APP_URL + 'style.css',
     APP_URL + 'head-init.js',
     APP_URL + 'dist/script.bundle.js',
+    APP_URL + 'logo.png',
     APP_URL + 'manifest.json',
     APP_URL + 'offline.html'
 ];
