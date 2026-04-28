e65266f56d4b6ec382093365453daaec977de05f
alessiocostanza3-ctrl <alessiocostanza3@gmail.com>
2026-04-28 22:19:54 +0200

    chore(sw): bump cache version and force client refresh

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 var GAS_URL = 'https://script.google.com/macros/s/AKfycbyVMV9MkGiqphN0AKXJdHXF0Arp1vxTYrCYi1SGv_4MKLRJkx--5HoGq7mmQX-p0ZTZ/exec';
 var APP_URL = 'https://alessiocostanza3-ctrl.github.io/gestion-produzione-ombre/';
 
-var SHELL_CACHE = 'prod-shell-v246';
+var SHELL_CACHE = 'prod-shell-v247';
 var SHELL_ASSETS = [
     APP_URL,
     APP_URL + 'index.html',
